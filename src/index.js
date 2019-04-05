94939f11c3bf54ad83f91f21f3ec4db0357a4922
Mosè Raguzzini <mose.raguzzini@gmail.com>
2019-04-05 17:32:18 +0200

    inital commit

---
+++ src/index.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import 'semantic-ui-css/components/accordion.min.css';
+import Styled from 'styled-components';
+import './style.css';
+import { Accordion } from 'semantic-ui-react';
+import _get from 'lodash/get';
+
+const routePattern = /to\":\"\/(.*?)\"/g;
+
+
+
+/**
+ *  An accordion menu based on React Semantic UI library
+ *
+ * @version 1.0.0
+ * @author [Mosè Raguzzini](https://github.com/bionicvapourboy)
+ */
+export default class SemanticAccordionMenu extends Component {
+
+  state = {
+    clicked: [],
+  }
+  
+  StyledAccordion = Styled(Accordion)`
+    background: transparent!important;
+
+    .title {
+      color: #fff!important;
+      .icon {
+        margin-right: 11px;
+      }
+    }
+
+    > .title, > .title.active, > .title:hover {
+      border-bottom: 1px solid ${this.props.separatorColor} !important;
+      background-color: ${this.props.firstLevelBackgroundColor}!important;
+    }
+
+    > .content.active {
+      background: ${this.props.submenuBackgroundColor};
+      border-bottom: 1px solid ${this.props.separatorColor} !important;
+    }
+    .accordion {
+      background: transparent!important;
+      color: ${this.props.submenuFontColor};
+      .title {
+        color: ${this.props.submenuFontColor}!important;
+      }
+
+          background: rgba(255,255,255,.3);
+    }
+
+    .accordion .title .dropdown.icon,
+    .title .dropdown.icon {
+      float: right!important;
+    }
+
+    .itemWrapper a {
+      padding: 10px 12px 10px 16px;
+      display: block;
+      margin-right: 3px;
+    }
+
+    .contentWrapper a {
+      padding: 10px;
+      padding-left: 16px;
+      display: block;
+      .icon{
+        margin-right: 11px;
+      }
+    }
+
+    .contentWrapper a.active, .itemWrapper a.active {
+      background-color: ${this.props.activeColor}
+    }
+    
+  `;
+  
+  /**
+   * Item style wrapper for custom accordions
+   */
+  ItemStyle = {
+    display: 'block',
+    position: 'relative',
+    margin: "-12px -16px",
+  }
+
+  /**
+   * Content style wrapper for custom accordion.content
+   */
+  ContentStyle = {
+    display: 'block',
+    position: 'relative',   
+  }
+
+  itemClick = (event) => {
+    this.setState({ clicked: []})
+
+    event.stopPropagation();
+  }
+  
+  /**
+   * Check for routes in child NavLink of react-router
+   */
+  checkChildRoutes = (obj) => JSON.stringify(obj).match(routePattern).map(route => route.split("\"")[2]);
+  
+
+
+  itemWrapper = (el) => ({ children: (<div className="itemWrapper" style={this.ItemStyle} onClick={this.itemClick}>{el}</div>)});
+  contentWrapper = (el) => (<div className="contentWrapper" style={this.ContentStyle} onClick={this.itemClick}>{el}</div>);
+  
+  getPanels = (objs) => objs.map(
+    (obj) => ({ key: obj.id, active: this.isActivePanel(obj), onTitleClick: this.onTitleClick, title: obj.item ? this.itemWrapper(obj.item) : obj.title, content: { content: this.getContent(obj)}})
+  )
+
+  onTitleClick = (e,data) => {
+    // console.log(e.target);
+    // data.active = !data.active;
+    console.log(e, data)
+    const clicked = [...this.state.clicked];
+    const index = clicked.indexOf(data.content);
+    if (index > -1) {
+      clicked.splice(index, 1);
+      this.setState({ clicked });
+    } else {
+      clicked.push(data.content);
+      this.setState({ clicked });
+    }
+  }
+
+  isActivePanel = (obj) => {
+    let result = false;
+    const childRoutes = this.checkChildRoutes(obj);
+    for(let i = 0; i < childRoutes.length; i += 1){
+      if(window.location.href.includes(childRoutes[i])){
+        result = true;
+      }
+    }
+    if(this.state.clicked.indexOf(obj.title) > -1 ) {
+      return !result;
+    } else {
+      return result;
+    }
+  }
+  
+  getContent = (obj) => Array.isArray(_get(obj, 'sections'))  ? <Accordion.Accordion panels={this.getPanels(obj.sections || null )} /> : this.contentWrapper(obj.content);
+
+  render() {
+    const StyledAccordion = this.StyledAccordion;
+    return (
+      <div style={{ width: this.props.width, fontSize: this.props.fontSize }} className="accordion-menu">
+        <StyledAccordion panels={this.getPanels(this.props.tree)} styled />
+      </div>
+    )
+  }
+}
+
+SemanticAccordionMenu.propTypes = {
+    /** An Array containing the configuration for menu tree */
+    tree: PropTypes.array,
+    /** Menu font size, default 13px */
+    fontSize: PropTypes.number,
+    /** Menu width, default 100% */
+    width: PropTypes.string,
+    /** SubMenu active content background color, default #ffffff */
+    submenuBackgroundColor: PropTypes.string,
+    /** SubMenu font color, default #000000 */
+    submenuFontColor: PropTypes.string,
+    /** Active color */
+    activeColor: PropTypes.string,
+    /** First level color */
+    firstLevelBackgroundColor:  PropTypes.string,
+
+  }
+
+SemanticAccordionMenu.defaultProps = {
+  tree: [],
+  fontSize: 13,
+  width: '100%',
+  submenuBackgroundColor: '#ffffff',
+  submenuFontColor: '#000000',
+  separatorColor: '#cccccc',
+  activeColor: '#266bc0',
+  firstLevelBackgroundColor: '#003178',
+};
+
+
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  plugins: [new BundleAnalyzerPlugin(), new UglifyJSPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: 'url-loader',
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024,
+              noquotes: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024,
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                enabled: false,
+                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+                // Try enabling it in your environment by switching the config to:
+                // enabled: true,
+                // progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+            },
+          },
+        ],
+      },
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
